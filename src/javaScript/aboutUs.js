e8a9245d667a731fb5e6183e6338370f19c47fe7
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2025-12-13 06:01:09 +0000

    init

---
+++ src/javaScript/aboutUs.js
@@ -0,0 +1,1 @@
+// scripts for the about us section
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './javaScript/header.js'; /* Importing header.js for header functionality */
+import './javaScript/hero.js'; /* Importing hero.js for hero section functionality */
+import './javaScript/petsList.js'; /* Importing petsList.js for pets list functionality */
+import './javaScript/aboutUs.js'; /* Importing aboutUs.js for about us section functionality */
+import './javaScript/FAQ.js'; /* Importing FAQ.js for FAQ section functionality */
+import './javaScript/successStories.js'; /* Importing successStories.js for success stories functionality */
+import './javaScript/footer.js'; /* Importing footer.js for footer functionality */
+
+import './javaScript/animalDetailsModal.js'; /* Importing animalDetailsModal.js for animal details modal functionality */
+import './javaScript/orderModal.js'; /* Importing orderModal.js for order modal functionality */
+
+
