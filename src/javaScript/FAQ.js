615e55eebf3bf9e383fa743ab630053569de8714
KateMal <katemalushko@gmail.com>
2025-12-14 17:18:05 +0100

    add faq

--- src/javaScript/FAQ.js
+++ src/javaScript/FAQ.js
@@ -1,1 +1,13 @@
 // scripts for the faq section
+
+const headers = document.querySelectorAll('.faq-accordion-header');
+
+headers.forEach(header => {
+  header.addEventListener('click', () => {
+    const icon = header.querySelector('.faq-accordion-icon');
+    const content = header.nextElementSibling;
+    
+    icon.classList.toggle('faq-accordion-icon--rotated');
+    content.classList.toggle('faq-accordion-content--visible');
+  });
+});
