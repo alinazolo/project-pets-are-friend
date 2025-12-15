aa4211c2edf039e3c3e916b8cf472295b11e7083
Oleh <vdovenko.oleg.1983@gmail.com>
2025-12-15 17:07:19 +0200

    create footer

--- src/javaScript/footer.js
+++ src/javaScript/footer.js
@@ -1,1 +1,10 @@
-// scripts for the footer section
+'use strict';
+const scrollToTop = document.querySelector('.scroll-to-top');
+window.addEventListener('scroll', function () {
+  window.scrollY > 300
+    ? scrollToTop.classList.add('visible')
+    : scrollToTop.classList.remove('visible');
+});
+scrollToTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
